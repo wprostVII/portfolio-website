7f54054cdc526ea6de6c78d81cb86197aefeb27c
wprostVII <192258874+wprostVII@users.noreply.github.com>
2025-02-20 00:26:15 +0100

    changed image size

--- src/components/galary/container.tsx
+++ src/components/galary/container.tsx
@@ -8,8 +8,8 @@ const GalaryContainer = () => {
                 Images.map((image, index) => {
                     return <Link href={image.src} prefetch key={`${index}_${image.name}`}>
                         <img 
-                            width={image.width ? image.width : 400} 
-                            height={image.height ? image.height : 400}
+                            width={image.width ? image.width : 600} 
+                            height={image.height ? image.height : 600}
                             src={image.src}
                             alt={image.alt}
                         />
