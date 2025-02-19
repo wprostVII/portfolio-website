3b00e8c67c86f26fb2f3fdca3b6c4d228272fa5e
wprostVII <192258874+wprostVII@users.noreply.github.com>
2025-02-20 00:54:00 +0100

    added image grayscale

--- src/components/galary/container.tsx
+++ src/components/galary/container.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const GalaryContainer = () => {
     return (
-        <div className="grid grid-cols-1 gap-2 relative px-6 py-2 overflow-y-scroll h-[94vh]"> 
+        <div className="grid grid-cols-1 gap-2 relative px-6 py-2 overflow-y-scroll h-[94vh] grayscale-[.5] hover:filter-none"> 
             {
                 Images.map((image, index) => {
                     return <Link href={image.src} prefetch key={`${index}_${image.name}`}>
